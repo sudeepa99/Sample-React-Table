3bf0c2d6ab846ca837523b97d8c65e5c4f76fdee
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-08-22 20:49:57 +0530

    used hook for fetching table data

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div className='bg-slate-950 h-screen w-screen'>
+        <h1 className='text-white text-4xl text-center pt-4'>Sample Table</h1>
+      
+    </div>
+  )
+}
